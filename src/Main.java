ef2c571e294d2c831d1da4b42ab9b71e0d32ad24
Anastasia <anastasia.m.2016@ya.ru>
2024-12-01 23:06:27 +0300

    домашнее задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Arrays;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Задача 1
+        System.out.println("\nЗадача 1:");
+        int[] weekExpenses = {1,5,6,3,2};
+        int monthExpenses = 0;
+        for (int exp:weekExpenses) {
+            monthExpenses+=exp;
+        }
+        System.out.println("Сумма трат за месяц составила "+monthExpenses+" рублей");
+        //Задача 2
+        System.out.println("\nЗадача 2:");
+        int minExp=weekExpenses[0];
+        int maxExp=weekExpenses[0];
+        for (int exp:weekExpenses) {
+            minExp=minExp>exp?exp:minExp;
+            maxExp=maxExp<exp?exp:maxExp;
+        }
+        System.out.println("Минимальная сумма трат за неделю составила  "+minExp+" рублей.\nМаксимальная сумма трат за неделю составила "+maxExp+" рублей");
+        //Задача 3
+        System.out.println("\nЗадача 3:");
+        double avgMonthExpenses=(double)monthExpenses/weekExpenses.length;
+        System.out.println("Средняя сумма трат за месяц составила  "+avgMonthExpenses+" рублей");
+        //Задача 4
+        System.out.println("\nЗадача 4:");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char tempBox;
+        for (int i=0;i<(reverseFullName.length-1)/2;i++){
+            tempBox=reverseFullName[i];
+            reverseFullName[i]=reverseFullName[reverseFullName.length-1-i];
+            reverseFullName[reverseFullName.length-1-i]=tempBox;
+        }
+        for (char l:reverseFullName){
+            System.out.print(l);
         }
     }
 }
